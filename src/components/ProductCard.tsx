ef2dd8502af6b3ea5303f172e4461eeaa2be55bb
Myles Smith <130503063+msmi1433@users.noreply.github.com>
2026-01-10 15:59:31 +0000

    render cards with rating

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,15 +1,17 @@
 import type { Product } from "../types";
+import StarRating from "./StarRating";
 
 type ProductCardProps = {
   product: Product;
 };
 
 const stockStatusTextMap = {
-  G: "In Stock",
+  G: "In stock",
 } as const;
 
 export default function ProductCard({ product }: ProductCardProps) {
-  const { productName, image, brand, price, stockStatus, score } = product;
+  const { productName, image, brand, price, stockStatus, score, reviewsCount } =
+    product;
 
   return (
     <div className="rounded space-y-4 bg-white">
@@ -18,7 +20,7 @@ export default function ProductCard({ product }: ProductCardProps) {
         alt={image.attributes.imageAltText}
         className="rounded"
       />
-      <div className="px-2 pb-4">
+      <div className="px-2 pb-4 space-y-2">
         <img
           className="max-w-15"
           src={brand.brandImage.url}
@@ -27,6 +29,10 @@ export default function ProductCard({ product }: ProductCardProps) {
         <p className="min-h-16">{productName}</p>
         <p className="text-lg font-bold text-red-600">£{price.priceIncTax}</p>
         <p className="text-sm">{stockStatusTextMap[stockStatus.status]}</p>
+        <div className="flex gap-2 items-center">
+          <StarRating score={score} />
+          <p className="text-sm text-neutral-500">{reviewsCount}</p>
+        </div>
       </div>
     </div>
   );
